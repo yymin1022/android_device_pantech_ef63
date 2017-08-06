fa3f234de16a5b9fbc6cfe2492d03ce806becd12
Chau Truong Thinh <chautruongthinh@gmail.com>
2017-08-06 10:53:49 +0000

    Fix firmware for A910SL_KIVN05042015
    Change-Id: I2acdc36f49c41535ade16a98a39a00838b1d352d

--- releasetools/device_check.sh
+++ releasetools/device_check.sh
@@ -1,19 +1,24 @@
 #!/sbin/sh
 
-CHECKSUM=`grep -c $'\x57' /dev/block/mmcblk0p5`
-CHECK_SS_SUM=275
-
 if grep -q "IM-A910S" /dev/block/mmcblk0p10 ; then
 
-	if [ "$CHECKSUM" = "$CHECK_SS_SUM" ] ; then 
-	mv /system/etc/firmware_ef63s/* /system/etc/firmware/
+	if grep -q "KIVN07172014" /dev/block/mmcblk0p10 ; then
+		mv /system/etc/firmware_ef63l/* /system/etc/firmware/
+	elif grep -q "KIVN05042015" /dev/block/mmcblk0p10 ; then
+		mv /system/etc/firmware_ef63l/* /system/etc/firmware/
+	else
+		mv /system/etc/firmware_ef63s/* /system/etc/firmware/
 	fi
 
 elif grep -q "IM-A910K" /dev/block/mmcblk0p10 ; then
 	mv /system/etc/firmware_ef63k/* /system/etc/firmware/
 
+elif grep -q "IM-A910L" /dev/block/mmcblk0p10 ; then
+	mv /system/etc/firmware_ef63l/* /system/etc/firmware/
 fi
 
 rm -rf /system/etc/firmware_ef63s
 rm -rf /system/etc/firmware_ef63k
+rm -rf /system/etc/firmware_ef63l
 rm -rf /system/bin/device_check.sh
+
