aa850e3160d940f0f5a7665fd4f05e9b2d8f3591
Chau Truong Thinh <chautruongthinh@gmail.com>
2018-06-15 13:15:47 +0700

    ef63: use libbase for log headers
    Change-Id: I59a72a0071ed0361949c57db4c8f0a80e29b13ae

--- init/Android.mk
+++ init/Android.mk
@@ -20,7 +20,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
-LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
+LOCAL_CFLAGS := -Wall.
+LOCAL_STATIC_LIBRARIES := libbase
 LOCAL_SRC_FILES := init_ef63.cpp
 LOCAL_MODULE := libinit_ef63
 
