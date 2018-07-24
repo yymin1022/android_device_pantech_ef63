3c562b7d6abd7972508a5bbc3a25351c0cd70955
Chau Truong Thinh <chautruongthinh@gmail.com>
2018-07-24 14:54:03 +0700

    ef63: fix correct firmware path
    Change-Id: Ic3e0bff2371a39f705c9d41979d488638606b044

--- releasetools/device_check.sh
+++ releasetools/device_check.sh
@@ -3,22 +3,22 @@
 if grep -q "IM-A910S" /dev/block/mmcblk0p10 ; then
 
 	if grep -q "KIVN07172014" /dev/block/mmcblk0p10 ; then
-		mv /system/vendor/firmware_ef63l/* /system/vendor/firmware/
+		mv /system/vendor/etc/firmware_ef63l/* /system/vendor/etc/firmware/
 	elif grep -q "KIVN05042015" /dev/block/mmcblk0p10 ; then
-		mv /system/vendor/firmware_ef63l/* /system/vendor/firmware/
+		mv /system/vendor/etc/firmware_ef63l/* /system/vendor/firmware/
 	else
-		mv /system/vendor/firmware_ef63s/* /system/vendor/firmware/
+		mv /system/vendor/etc/firmware_ef63s/* /system/vendor/firmware/
 	fi
 
 elif grep -q "IM-A910K" /dev/block/mmcblk0p10 ; then
-	mv /system/vendor/firmware_ef63k/* /system/vendor/firmware/
+	mv /system/vendor/etc/firmware_ef63k/* /system/vendor/firmware/
 
 elif grep -q "IM-A910L" /dev/block/mmcblk0p10 ; then
-	mv /system/vendor/firmware_ef63l/* /system/vendor/firmware/
+	mv /system/vendor/etc/firmware_ef63l/* /system/vendor/firmware/
 fi
 
-rm -rf /system/vendor/firmware_ef63s
-rm -rf /system/vendor/firmware_ef63k
-rm -rf /system/vendor/firmware_ef63l
+rm -rf /system/vendor/etc/firmware_ef63s
+rm -rf /system/vendor/etc/firmware_ef63k
+rm -rf /system/vendor/etc/firmware_ef63l
 rm -rf /system/bin/device_check.sh
 
